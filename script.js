fee76852e315ffef31091c30b71c0eb7b62f0955
skimmynz <skimmynz@gmail.com>
2025-12-13 16:30:25 +1100

    Update script.js

--- script.js
+++ script.js
@@ -370,44 +370,75 @@ function toggleDropdown(entryId) {
 const optimizedRenderer = new OptimizedRenderer();
 let currentData = null;
 
-const RAIDS = {
-  msv: {
-    short: 'MSV',
-    name: "Mogu'shan Vaults",
-    encounters: {
-      "The Stone Guard": 1395,
-      "Feng the Accursed": 1390,
-      "Gara'jal the Spiritbinder": 1434,
-      "The Spirit Kings": 1436,
-      "Elegon": 1500,
-      "Will of the Emperor": 1407,
-    },
+
+const TIERS = {
+  t14: {
+    name: 'T14',
+    raids: {
+      msv: {
+        short: 'MSV',
+        name: "Mogu'shan Vaults",
+        encounters: {
+          "The Stone Guard": 1395,
+          "Feng the Accursed": 1390,
+          "Gara'jal the Spiritbinder": 1434,
+          "The Spirit Kings": 1436,
+          "Elegon": 1500,
+          "Will of the Emperor": 1407,
+        },
+      },
+      hof: {
+        short: 'HoF',
+        name: 'Heart of Fear',
+        encounters: {
+          "Imperial Vizier Zor'lok": 1507,
+          "Blade Lord Ta'yak": 1504,
+          "Garalon": 1463,
+          "Wind Lord Mel'jarak": 1498,
+          "Amber-Shaper Un'sok": 1499,
+          "Grand Empress Shek'zeer": 1501,
+        },
+      },
+      toes: { 
+        short: 'ToES', 
+        name: 'Terrace of Endless Spring', 
+        encounters: {
+          "Protectors of the Endless": 1409,
+          "Tsulong": 1505,
+          "Lei Shi": 1506,
+          "Sha of Fear": 1431,
+        }
+      },
+    }
   },
-  hof: {
-    short: 'HoF',
-    name: 'Heart of Fear',
-    encounters: {
-      "Imperial Vizier Zor'lok": 1507,
-      "Blade Lord Ta'yak": 1504,
-      "Garalon": 1463,
-      "Wind Lord Mel'jarak": 1498,
-      "Amber-Shaper Un'sok": 1499,
-      "Grand Empress Shek'zeer": 1501,
-    },
-  },
-  toes: { 
-    short: 'ToES', 
-    name: 'Terrace of Endless Spring', 
-    encounters: {
-      "Protectors of the Endless": 1409,
-      "Tsulong": 1505,
-      "Lei Shi": 1506,
-      "Sha of Fear": 1431,
+  t15: {
+    name: 'T15',
+    raids: {
+      tot: {
+        short: 'ToT',
+        name: 'Throne of Thunder',
+        encounters: {
+          "Jin'rokh the Breaker": 51577,
+          "Horridon": 51575,
+          "Council of Elders": 51570,
+          "Tortos": 51565,
+          "Megaera": 51578,
+          "Ji-Kun": 51573,
+          "Durumu the Forgotten": 51572,
+          "Primordius": 51574,
+          "Dark Animus": 51576,
+          "Iron Qon": 51559,
+          "Twin Consorts": 51560,
+          "Lei Shen": 51579,
+          "Ra-den": 51580,
+        },
+      },
     }
   },
 };
 
-let currentRaidKey = 'msv';
+let currentTierKey = 't15';
+let currentRaidKey = 'tot';
 
 // DOM refs with null checks
 const raidMenu = document.getElementById('raid-menu');
